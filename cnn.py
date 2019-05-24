5cd7193bf8f6787f73ec8642d363fff8cf3dccd7
stolikparanoik <47759484+stolikparanoik@users.noreply.github.com>
2019-05-24 19:39:53 +0200

    Add files via upload

---
+++ cnn.py
@@ -0,0 +1,84 @@
+from keras import layers
+from keras import models
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+model = models.Sequential()
+model.add(layers.Conv2D(32,(3,3), activation='relu', input_shape=(150,150,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(128,(3,3),activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(128,(3,3),activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512,activation='relu'))
+model.add(layers.Dense(4, activation='softmax'))
+model.summary()
+
+from keras import optimizers
+
+model.compile(loss='categorical_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+
+from keras.preprocessing.image import ImageDataGenerator
+import os
+
+train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=0.2,
+                                   height_shift_range=0.2, shear_range=0.2, zoom_range=0.2, horizontal_flip=True,)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+original_dataset_dir  = './data/train'
+base_dir ='./split_data'
+#os.mkdir(base_dir)
+
+train_dir = os.path.join(base_dir,'train')
+#os.mkdir(train_dir)
+validation_dir = os.path.join(base_dir,'validation')
+#os.mkdir(validation_dir)
+test_dir = os.path.join(base_dir,'test')
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(150,150),
+        batch_size=32,
+        class_mode='categorical'
+)
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150,150),
+        batch_size=32,
+        class_mode='categorical'
+)
+
+for data_batch, labels_batch in train_generator:
+    print('data batch shape: ', data_batch.shape)
+    print('labels batch shape: ', labels_batch.shape)
+    break
+
+history = model.fit_generator(
+     train_generator,
+     steps_per_epoch=100,
+     epochs=30,
+     validation_data=validation_generator,
+     validation_steps=50
+)
+model.save('modelcool30.h5')
+
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validating loss')
+plt.legend()
+
+plt.show()
